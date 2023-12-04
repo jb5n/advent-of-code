afc447c0c39619386311e267df232955891a5de4
Justin Bostian <jrbostian@gmail.com>
2023-12-04 10:48:06 -0600

    Day 4

--- 2023/day_4/source.py
+++ 2023/day_4/source.py
@@ -1,18 +1,36 @@
 from pathlib import WindowsPath
+import math
 
 input_path = WindowsPath(__file__).parent / 'input.txt'
 
 def part1():
-	input = ''
+	lines = []
 	with open(input_path) as f:
-		input = f.read()
-	return input
+		lines = f.readlines()
+		
+	return sum([math.floor(2 ** (len(set(filter(None, line.replace('\n', '').split(':')[1].split('|')[0].split(' '))) & set(filter(None, line.replace('\n', '').split(':')[1].split('|')[1].split(' ')))) - 1)) for line in lines])
 
 def part2():
-	input = ''
+	lines = []
 	with open(input_path) as f:
-		input = f.read()
-	return input
+		lines = f.readlines()
+	
+	card_wins = []
+	
+	def score_card(card_index):
+		if len(card_wins[card_index]) == 0:
+			return 1
+		else:
+			return sum([score_card(i) for i in card_wins[card_index]]) + 1
+	
+	for i, line in enumerate(lines):
+		win_count = len(set(filter(None, line.replace('\n', '').split(':')[1].split('|')[0].split(' '))) & set(filter(None, line.replace('\n', '').split(':')[1].split('|')[1].split(' '))))
+		card_wins.append(list(range(i + 1, i + win_count + 1)))
+	
+	wins_total = 0
+	for i in range(len(lines)):
+		wins_total += score_card(i)
+	return wins_total
 
 print(part1())
 print(part2())
